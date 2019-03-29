1d747b823772b69e063f03fbfbc44c433117e0c5
Mario Kratafila <Mario_Kratafila@epam.com>
2019-03-29 10:07:03 +0100

    App.js updated

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Changed!
+            PLEASE STAND BY
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://fallout.bethesda.net"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Vault-Tec Corporation 
           </a>
         </header>
       </div>
